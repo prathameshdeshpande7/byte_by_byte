2e4e3f5b5803f5762268eb46c59c1640605e928c
Prathamesh Deshpande <prathameshdeshpande7@gmail.com>
2019-04-05 20:46:42 +0530

    Intermediate checkin

--- 28_Sort_Stacks/sort_stacks.c
+++ 28_Sort_Stacks/sort_stacks.c
@@ -43,12 +43,16 @@ int pop(STACK *stack)
 	return temp;
 }
 
+int top(STACK *stack)
+{
+	return stack->top == -1 ? -1 : stack->stack_ptr[stack->top];
+}
+
 void create_stack(STACK *stack)
 {
 	stack->stack_ptr = (int *)malloc(MAX_STACK_SIZE * sizeof(int));
 	assert(stack->stack_ptr);
 	stack->top = -1;
-	return stack;
 }
 
 void destroy_stack(STACK *stack)
@@ -56,21 +60,43 @@ void destroy_stack(STACK *stack)
 	free(stack->stack_ptr);
 }
 
+void sort_stack(STACK *s1, STACK *s2)
+{
+	int temp = 0;
+
+	while(temp = pop(s1)) {
+		if (!is_empty(s2))
+			push(s2, temp);
+
+		if (temp < top(s2)) {
+			push(s2, temp);
+		}
+	}
+}
+
 int main()
 {
-	int arr[] = {1, 2, 3, 4};
+	int arr[] = {1, 2, 3, 4, 5, 10, 9, 8, 7, 6};
 	int i, len_arr = sizeof(arr)/sizeof(arr[0]);
 	STACK s1, s2;
 
 	create_stack(&s1);
 	create_stack(&s2);
 
+	printf("Stack: ");
+	for (i = len_arr - 1; i >= 0; i--)
+		printf("%d ", arr[i]);
+
+	printf("\n");
+
 	for (i = 0; i < len_arr; i++)
 		push(&s1, arr[i]);
 
 
-	while(!is_empty(&s1))
-		printf("%d ", pop(&s1));
+	sort_stack(&s1, &s2);
+
+	while(!is_empty(&s2))
+		printf("%d ", pop(&s2));
 
 	destroy_stack(&s2);
 	destroy_stack(&s1);
